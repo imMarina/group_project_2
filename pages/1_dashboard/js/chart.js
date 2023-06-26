f2f78c77c5f7d19ac92d782361a8c24bce8d64e5
Anastasiya Melnikova (work) <sav20043@yandex.ru>
2023-06-26 16:04:32 +0300

    Добавила график в раздел dashboard: visitors

--- pages/1_dashboard/js/chart.js
+++ pages/1_dashboard/js/chart.js
@@ -1,58 +1,32 @@
-let chart = JSC.chart('chartDiv', { 
-  debug: true, 
-  legend_position: 'bottom right', 
-  type: 'area spline', 
-  defaultSeries: { shape_opacity: 0.5 }, 
-  xAxis: { 
-    crosshair_enabled: true, 
-    scale: { type: 'time' } 
-  }, 
-  title_label_text: 'Costs (Last 6 Months)', 
-  yAxis: { formatString: 'c' }, 
-  series: [ 
-    { 
-      name: 'Purchases', 
-      points: [ 
-        ['1/1/2020', 29.9], 
-        ['2/1/2020', 97.5], 
-        ['3/1/2020', 110.4], 
-        ['4/1/2020', 129.2], 
-        ['5/1/2020', 144.0], 
-        ['6/1/2020', 176.0] 
-      ] 
-    }, 
-    { 
-      name: 'Taxes', 
-      points: [ 
-        ['1/1/2020', 86.9], 
-        ['2/1/2020', 79.5], 
-        ['3/1/2020', 95.4], 
-        ['4/1/2020', 97.2], 
-        ['5/1/2020', 123.0], 
-        ['6/1/2020', 111.0] 
-      ] 
-    }, 
-    { 
-      name: 'Supplies', 
-      points: [ 
-        ['1/1/2020', 129.9], 
-        ['2/1/2020', 111.5], 
-        ['3/1/2020', 66.4], 
-        ['4/1/2020', 29.2], 
-        ['5/1/2020', 88.0], 
-        ['6/1/2020', 102.0] 
-      ] 
-    }, 
-    { 
-      name: 'Rent', 
-      points: [ 
-        ['1/1/2020', 56.9], 
-        ['2/1/2020', 56.5], 
-        ['3/1/2020', 56.4], 
-        ['4/1/2020', 56.2], 
-        ['5/1/2020', 75.0], 
-        ['6/1/2020', 56.0] 
-      ] 
-    } 
-  ] 
-}); 
+var visitCanvas = document.getElementById("visitChart");
+
+
+Chart.defaults.font.family = "Teko";
+Chart.defaults.font.size = 22;
+Chart.defaults.color = "black";
+
+let visitData = {
+  labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
+  datasets: [{
+    label: "Car Speed (mph)",
+    data: [0, 59, 75, 20, 20, 55, 40],
+    tension: 0.4,
+    cubicInterpolationMode: 'monotone',
+    fill: false,
+    borderColor: '#E64A19',
+    backgroundColor: 'transparent',
+    borderDash: [20, 10, 60, 10],
+    pointBorderColor: '#E64A19',
+    pointBackgroundColor: '#FFA726',
+    pointRadius: 5,
+    pointHoverRadius: 10,
+    pointHitRadius: 30,
+    pointBorderWidth: 4,
+    pointStyle: 'rectRounded'
+  }]
+};
+
+let lineChart = new Chart(visitCanvas, {
+  type: 'line',
+  data: visitData
+});
